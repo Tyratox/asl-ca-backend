0733130e9ccb9ad174d2403710898bd14d5bf375
Nico Hauser <me@tyratox.ch>
2021-11-10 14:41:20 +0100

    Update build-ca-utility.sh

--- build-ca-utility.sh
+++ build-ca-utility.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 
+if [ -z "$1" ]
+then
+  echo "Usage: ./build-ca-utility.sh /input/ca-utility.cpp /output/ca-utility /path/to/CA /path/to/openssl.cnf /path/to/openssl"
+  exit
+fi
+
+if [ -z "$2" ]
+then
+  echo "Output path is required!"
+  exit
+fi
+
+if [ -z "$3" ]
+then
+  echo "CA path is required!"
+  exit
+fi
+
+if [ -z "$4" ]
+then
+  echo "OpenSSL config path is required!"
+  exit
+fi
+
+if [ -z "$5" ]
+then
+  echo "OpenSSL path is required!"
+  exit
+fi
+
 g++ -std=c++17 -O3 -Wall "$1" -o "$2" -DCA_PATH="$3" -DCONFIG_PATH="$4" -DOPENSSL_PATH="$5"
