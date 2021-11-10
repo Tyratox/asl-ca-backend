31533b12fcc9e5c78040561fa0e8e97a67878bec
Nico Hauser <me@tyratox.ch>
2021-11-10 14:47:13 +0100

    Fix error messages

--- src/ca-utility.cpp
+++ src/ca-utility.cpp
@@ -35,8 +35,8 @@ string exec(const char* cmd) {
 int main(int argc, char *argv[]){
 
   if(argc <= 1){
-    cout << "ca-utility requires at least three arguments!" << endl;
-    cout << "Usage: ./ca-utility /path/to/openssl.conf (generate|request|sign|revoke) target" << endl;
+    cout << "ca-utility requires at least one argument!" << endl;
+    cout << "Usage: ./ca-utility (generate|request|sign|revoke) target OR ./ca-utility update-crl" << endl;
     return 1;
   }
   string configPath = CONFIG_PATH_STRING;
@@ -110,8 +110,8 @@ int main(int argc, char *argv[]){
     
     return 0;
   }else if(argc <= 2){
-    cout << "ca-utility requires at least three arguments!" << endl;
-    cout << "Usage: ./ca-utility /path/to/openssl.conf (generate|request|sign|revoke) target" << endl;
+    cout << "ca-utility requires at least two arguments!" << endl;
+    cout << "Usage: ./ca-utility (generate|request|sign|revoke) target" << endl;
     return 1;
   }
 
