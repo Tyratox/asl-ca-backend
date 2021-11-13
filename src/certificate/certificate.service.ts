3cc686123cf5e1c6cc574503a2054dc23096410a
Nico Hauser <me@tyratox.ch>
2021-11-13 17:30:00 +0100

    Fix race condition

--- src/certificate/certificate.service.ts
+++ src/certificate/certificate.service.ts
@@ -49,79 +49,75 @@ export class CertificateService {
     name: string,
     password: string,
   ) {
-    return new Promise((resolve, reject) => {
-      this.lock.acquire('generateCertificateForUser', () => {
-        const certificate = this.certificateRepository.create({
-          name,
-          is_revoked: false,
-          user,
-        });
-
-        resolve(
-          this.certificateRepository.save(certificate).then((certificate) => {
-            const certificateId = certificate.id.toString();
-
-            const CA_PATH = this.configService.get<string>('CA_PATH');
-            const CA_UTIL_PATH = join(CA_PATH, 'ca-utility');
-
-            const TEMP_PATH_CRT = join(CA_PATH, 'tmp', certificateId + '.crt');
-            const TEMP_PATH_KEY = join(CA_PATH, 'tmp', certificateId + '.key');
-            const TEMP_PATH_P12 = join(CA_PATH, 'tmp', certificateId + '.p12');
-
-            // IMPORTANT: DON'T ENABLE THE SHELL OPTION, WE HAVE USER CONTROLLED INPUT
-            const keyFileInBase64 = execFileSync(
-              CA_UTIL_PATH,
-              ['generate', certificateId],
-              {
-                encoding: 'base64',
-                stdio: 'pipe',
-              },
-            );
-            execFileSync(CA_UTIL_PATH, ['request', certificateId, user.email], {
-              stdio: 'pipe',
-            });
-            const certFileInBase64 = execFileSync(
-              CA_UTIL_PATH,
-              ['sign', certificateId],
-              { encoding: 'base64', stdio: 'pipe' },
-            );
-            // write to tmp directory, we have it in memory anyway so it's probably okay
-            // to write it to a directory for generating the p12 file
-            writeFileSync(TEMP_PATH_KEY, keyFileInBase64, {
-              encoding: 'base64',
-            });
-            writeFileSync(TEMP_PATH_CRT, certFileInBase64, {
-              encoding: 'base64',
-            });
-
-            execFileSync(
-              'openssl',
-              [
-                'pkcs12',
-                '-export',
-                '-in',
-                TEMP_PATH_CRT,
-                '-inkey',
-                TEMP_PATH_KEY,
-                '-out',
-                TEMP_PATH_P12,
-                '-passout',
-                /* empty password */
-                `pass:${password}`,
-              ],
-              { stdio: 'pipe' },
-            );
-            // read p12 file
-            const p12 = readFileSync(TEMP_PATH_P12, { encoding: 'base64' });
-            // delete the key, cert and p12 file
-            unlinkSync(TEMP_PATH_KEY);
-            unlinkSync(TEMP_PATH_CRT);
-            unlinkSync(TEMP_PATH_P12);
-
-            return { certificate, p12 };
-          }),
-        );
+    return this.lock.acquire('generateCertificateForUser', async () => {
+      let certificate = this.certificateRepository.create({
+        name,
+        is_revoked: false,
+        user,
       });
+
+      certificate = await this.certificateRepository.save(certificate);
+
+      const certificateId = certificate.id.toString();
+
+      const CA_PATH = this.configService.get<string>('CA_PATH');
+      const CA_UTIL_PATH = join(CA_PATH, 'ca-utility');
+
+      const TEMP_PATH_CRT = join(CA_PATH, 'tmp', certificateId + '.crt');
+      const TEMP_PATH_KEY = join(CA_PATH, 'tmp', certificateId + '.key');
+      const TEMP_PATH_P12 = join(CA_PATH, 'tmp', certificateId + '.p12');
+
+      // IMPORTANT: DON'T ENABLE THE SHELL OPTION, WE HAVE USER CONTROLLED INPUT
+      const keyFileInBase64 = execFileSync(
+        CA_UTIL_PATH,
+        ['generate', certificateId],
+        {
+          encoding: 'base64',
+          stdio: 'pipe',
+        },
+      );
+      execFileSync(CA_UTIL_PATH, ['request', certificateId, user.email], {
+        stdio: 'pipe',
+      });
+      const certFileInBase64 = execFileSync(
+        CA_UTIL_PATH,
+        ['sign', certificateId],
+        { encoding: 'base64', stdio: 'pipe' },
+      );
+      // write to tmp directory, we have it in memory anyway so it's probably okay
+      // to write it to a directory for generating the p12 file
+      writeFileSync(TEMP_PATH_KEY, keyFileInBase64, {
+        encoding: 'base64',
+      });
+      writeFileSync(TEMP_PATH_CRT, certFileInBase64, {
+        encoding: 'base64',
+      });
+
+      execFileSync(
+        'openssl',
+        [
+          'pkcs12',
+          '-export',
+          '-in',
+          TEMP_PATH_CRT,
+          '-inkey',
+          TEMP_PATH_KEY,
+          '-out',
+          TEMP_PATH_P12,
+          '-passout',
+          /* empty password */
+          `pass:${password}`,
+        ],
+        { stdio: 'pipe' },
+      );
+      // read p12 file
+      const p12 = readFileSync(TEMP_PATH_P12, { encoding: 'base64' });
+      // delete the key, cert and p12 file
+      unlinkSync(TEMP_PATH_KEY);
+      unlinkSync(TEMP_PATH_CRT);
+      unlinkSync(TEMP_PATH_P12);
+
+      return { certificate, p12 };
     });
   }
 
@@ -141,20 +137,18 @@ export class CertificateService {
   }
 
   async revokeCertificate(certificate: CertificateEntity) {
-    return new Promise((resolve, reject) => {
-      this.lock.acquire('revokeCertificate', () => {
-        const CA_PATH = this.configService.get<string>('CA_PATH');
-        const CA_UTIL_PATH = join(CA_PATH, 'ca-utility');
-        const certificateId = certificate.id.toString();
+    return this.lock.acquire('revokeCertificate', () => {
+      const CA_PATH = this.configService.get<string>('CA_PATH');
+      const CA_UTIL_PATH = join(CA_PATH, 'ca-utility');
+      const certificateId = certificate.id.toString();
 
-        execFileSync(CA_UTIL_PATH, ['revoke', certificateId]);
-        execFileSync(CA_UTIL_PATH, ['update-crl']);
+      execFileSync(CA_UTIL_PATH, ['revoke', certificateId], { stdio: 'pipe' });
+      execFileSync(CA_UTIL_PATH, ['update-crl'], { stdio: 'pipe' });
 
-        // if system calls succeeded, also store in database
-        certificate.is_revoked = true;
+      // if system calls succeeded, also store in database
+      certificate.is_revoked = true;
 
-        resolve(this.certificateRepository.save(certificate));
-      });
+      return this.certificateRepository.save(certificate);
     });
   }
 }
